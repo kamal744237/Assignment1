446c9140ec87a863e477f18e0c678c2fb81d65be
Kamaljeet Kaur Na <744237@student.tor.loc>
2020-01-20 14:38:19 -0500

    2:38 pm commit

---
+++ kamal/kamal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kamal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
